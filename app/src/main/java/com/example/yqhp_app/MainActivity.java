ae90801a2b9e25fe355dcb974491a18cf7d3717a
24meomeo <meomeo2483@gmail.com>
2023-09-27 21:18:54 +0700

    them thu vien, conect voi firebase voi trang mo dau

--- app/src/main/java/com/example/yqhp_app/MainActivity.java
+++ app/src/main/java/com/example/yqhp_app/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.yqhp_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler hander = new Handler();
+        hander.postDelayed(new Runnable() {
+            @Override
+            public void run() { nextActivity(); }
+
+            }, 2000);
+    }
+
+    private void nextActivity() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Intent intent = new Intent(this, dangnhap.class);
+        startActivity(intent);
+
     }
 }
